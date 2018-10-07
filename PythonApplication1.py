98f85b7527377cc78da778b51356393ed4470931
Almidaa <43917869+Almidaa@users.noreply.github.com>
2018-10-07 15:19:50 +0100

    Added a bunch of new things
    - Added food.
    - Added wall collision
    - Added body segments
    
    - Problems I found -
    
    It looks like everytime I eat an apple, the segment get inside the head. So I added "segments[0].color("white")"(line 114) in the segments lines of code. That way, the first segment that gets created inside the head is white (the original head color) and the other segments (the body) are black.
    
    Another problem is that when I start the game, the first apple is always on this coordinates (0, 100) and I don't know a way to put it in a random place on the canvas when I start a new game.
    
    
    Sorry if I'm bad at this. It's my first time programming and using this website. I'm trying to learn and integrate in this society. Btw, I'm only 15.

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,9 +1,9 @@
 # Simple Snake Game
 #thx TokyoEdThec
-#Part 1: Getting Started
 
 import turtle
 import time
+import random
 
 delay = 0.5
 points = 0
@@ -24,6 +24,17 @@ head.penup()
 head.goto(0,0)
 head.direction = "left"
 head.onscreenclick(lambda x, y: turtle.goto(x, y) or print(turtle.xcor(), turtle.ycor())
+                   
+# Snake food
+
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)  # Needs to be corrected. Initial postiion should be random and not (0, 100)
+                   
+segments = []
 
 #Functions
 def move():
@@ -42,22 +53,79 @@ def move():
     if head.direction == "left":
         x = head.xcor()
         head.setx(x-20)
+                   
+ def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
+def go_right():
+    head.direction = "right"
+
+def go_left():
+    head.direction = "left"                  
+                   
+ # Keyboard binds
+
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_right, "d")
+wn.onkeypress(go_left, "a")                  
 
 
-def increment_points():
-    global points
-    points += 1
-
-#Main Gme loop
 
 
-while points < 10:
+# Main Game loop
+
+while True:
     wn.update()
 
-    move()
-    increment_points()
-    print(head.position())
+    # Check for a collision with the border
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+
+        #Clear the segments list
+        segments.clear()
+
+    move ()
+
+    # Collsion with the food
+    if head.distance(food) < 20:
+        # Move the food to a random spot on the screnn
+        x=random.randint(-290, 290)
+        y=random.randint(-290, 290)
+        food.goto(x, y)
+
+        #New segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("black")
+        new_segment.penup()
+        segments.append(new_segment)
+        segments[0].color("white")
+
+    # Move the end segments first in reverse
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+    
+           
     time.sleep(delay)
 
 wn.mainloop()
-
